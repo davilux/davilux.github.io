2cc085ef49a1638bbfeffc59b7dc93af084727b9
Davi Lux <mx.davilux@gmail.com>
2023-01-18 16:40:08 -0500

    conditional styling for nav links

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,41 +1,23 @@
 import './index.css'
-import {Link} from 'react-router-dom'
+import {Link, useLocation} from 'react-router-dom'
 import {NavHashLink} from 'react-router-hash-link'
 
 const Navbar = () => {
+    let location = useLocation();
+
     return(
         <div className='nav-bar'>
              <nav>
+                <NavHashLink className={`${location.pathname}${location.hash}` === '/#about' ? "activeNavHashLink" : "inactiveNavHashLink"} smooth to="/#about">About</NavHashLink>
+                <NavHashLink className={`${location.pathname}${location.hash}` === '/#projects' ? "activeNavHashLink" : "inactiveNavHashLink"}  smooth to="/#projects">Projects</NavHashLink>
+                <NavHashLink className={`${location.pathname}${location.hash}` === '/#contact' ? "activeNavHashLink" : "inactiveNavHashLink"}  smooth to="/#contact">Contact</NavHashLink>
 
-                {/* <NavHashLink className='inactiveNavHashLink' smooth to="/#about">About</NavHashLink>
-                <NavHashLink className='inactiveNavHashLink' smooth to="/#projects">Projects</NavHashLink>
-                <NavHashLink className='inactiveNavHashLink' smooth to="/#contact">Contact</NavHashLink>
-                <Link className='resumeLink' to="/resume">Resume</Link> */}
-
-
-                <NavHashLink className={(navData) => (checkActive(navData) ? 'activeNavHashLink' : 'inactiveNavHashLink')} smooth to="/#about">About</NavHashLink>
-                <NavHashLink className={(navData) => (checkActive(navData) ? 'activeNavHashLink' : 'inactiveNavHashLink')}  smooth to="/#projects">Projects</NavHashLink>
-                <NavHashLink className={(navData) => (checkActive(navData) ? 'activeNavHashLink' : 'inactiveNavHashLink')}  smooth to="/#contact">Contact</NavHashLink>
                 <Link className='resumeLink' to="/resume">Resume</Link>
             </nav>
-
-{/* TODO: Selective styling for when a link is active. */}
-{/* Looks like someone else is having this exact issue. Posted very recently:
-
-https://stackoverflow.com/questions/72518534/navhashlink-warning-react-does-not-recognize-the-isactive-prop-on-a-dom#new-answer */}
         </div>
+
+        // TODO: Make sure link becomes active/inactive when you scroll to/away from a section (not just when you click a link)
     )
-}
-
-const checkActive = (match, location)=> {
-
-    // Don't know how to get the match and location data here using 'react-router-hash-link', because using an isActive function was for react-router-dom, not react-router-hash-link...
-
-    // https://stackoverflow.com/questions/47879663/root-navlink-always-get-active-class-react-router-dom
-
-    if(!location) return false;
-    const {pathname} = location;
-    return pathname === "/";
 }
 
 export default Navbar
