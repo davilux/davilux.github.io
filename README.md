3f603d3239dfa99cc1e025a627de102ee66f03fa
Davi Lux <mx.davilux@gmail.com>
2023-01-19 17:47:28 -0500

    Contact form connected to emailJS

--- README.md
+++ README.md
@@ -6,16 +6,18 @@ MVP: User stories. As a user, I can…
 Tier 1
 
 - [x]  scroll through a single page to view about, projects, and contact sections
-- [ ]  click navigation links which will scroll to each of the given sections
+- [x]  click navigation links which will scroll to each of the given sections
 - [ ]  view and/or download resume (via link in navbar)
 
 - [ ]  page through different projects to read about them
 - [ ]  click links to view the deployed sites and project GitHubs
 
-- [ ]  submit a contact form
+- [x]  submit a contact form
+- [ ]  read a helpful message confirming email was sent
 
 - [ ]  view the site on mobile, tablet, and desktop
 
 Tier 2
 
 - [ ]  see which section is currently in view by the navbar
+- [ ]  easily switch between light and dark modes
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,25 +1,45 @@
 import "./index.css"
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
+import emailjs from '@emailjs/browser';
+import { clear } from "@testing-library/user-event/dist/clear";
 
 const Contact = () => {
   const [email, setEmail] = useState("")
   const [name, setName] = useState("")
   const [subject, setSubject] = useState("")
   const [message, setMessage] = useState("")
-
   const [errorMessages, setErrorMessages] = useState([])
   const [showErrors, setShowErrors] = useState(false)
+  const [emailSent, setEmailSent] = useState(false)
 
-  const handleFormSubmission = () => {
-    if (validateForm()) {
-      //connect to back end here. Submit the email.
-      return true
+  const form = useRef();
+  const nameField = useRef();
+  const emailField = useRef();
+  const subjectField = useRef();
+  const messageField = useRef();
+
+
+  const handleFormSubmission = (e) => {
+    if (validateForm()){
+      sendEmail(e)
+      clearForm()
+      setEmailSent(true)
     }
-    return false
   }
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('gmail_id', 'contact_form', form.current, '1xN_G2VM06GatQ0GB')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   const validateEmail = (email) => {
-    //TODO: validate email on the back end... using a regex here is not sufficient
+    //TODO: validate email. Ideally, we'd want to do this on the back end, but for the sake of this app, I can probably get a working version with regEx.
 
     //temp:
     return true
@@ -34,6 +54,7 @@ const Contact = () => {
     if (email === "") errors.push("Email required.")
     if (subject === "") errors.push("Subject required.")
     if (message === "") errors.push("Message must not be empty.")
+
     if (errors.length > 0) {
       setShowErrors(true)
       setErrorMessages(errors)
@@ -43,6 +64,14 @@ const Contact = () => {
       return true
     }
   }
+
+  const clearForm = () => {
+    nameField.current.value = ''
+    emailField.current.value = ''
+    subjectField.current.value = ''
+    messageField.current.value = ''
+  }
+
 
   return (
     <section id="contact" className="contact">
@@ -54,36 +83,45 @@ const Contact = () => {
           Feel free to send me an email with job opportunities or interest in
           collaboration.
         </p>
-        <form>
+        <form ref={form}>
           <section className="top-inputs">
           <input
             type="text"
             className = "form-name"
             placeholder="name"
+            name="user_name"
+            ref = {nameField}
             onChange={(e) => setName(e.target.value)}
           />
           <input
             type="email"
             className = "form-email"
             placeholder="email"
+            name="user_email"
+            ref = {emailField}
             onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="text"
             className = "form-subject"
             placeholder="subject"
+            name="subject"
+            ref = {subjectField}
             onChange={(e) => setSubject(e.target.value)}
           />
           </section>
           <textarea
             type="text"
             placeholder="message"
+            name="message"
+            ref = {messageField}
             onChange={(e) => setMessage(e.target.value)}
           />
           {showErrors && <ul> {errorMessages.map((item, index) => {
                 return <li key={index}>{item}</li>
               })
             } </ul>}
+          {emailSent && <p>Email sent!</p>}
 
           <button
             color="primary"
@@ -106,3 +144,8 @@ const Contact = () => {
 }
 
 export default Contact
+
+
+//TODO: Integrate reCaptcha to verify user is human.
+//TODO: Keep fields styled even when they are filled with autocomplete
+//TODO: User can submit the form using enter key.
