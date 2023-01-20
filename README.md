f8a0f68c3730c2501d11f4cadbb2367a95728410
Davi Lux <mx.davilux@gmail.com>
2023-01-20 12:34:39 -0500

    Projects icons change to reflect current selection, gh and deployed links added.

--- README.md
+++ README.md
@@ -9,11 +9,13 @@ Tier 1
 - [x]  click navigation links which will scroll to each of the given sections
 - [ ]  view and/or download resume (via link in navbar)
 
+- [ ]  read about Davi
+
 - [ ]  page through different projects to read about them
 - [ ]  click links to view the deployed sites and project GitHubs
 
 - [x]  submit a contact form
-- [ ]  read a helpful message confirming email was sent
+- [x]  read a helpful message confirming email was sent
 
 - [ ]  view the site on mobile, tablet, and desktop
 
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,8 +1,35 @@
 import "./index.css"
 import FolderIcon from "@mui/icons-material/Folder"
 import FolderOpenIcon from "@mui/icons-material/FolderOpen"
+import { useState } from 'react';
+
+class Project {
+  constructor(name, id, description, ghLink, deployedLink){
+    this.name = name;
+    this.id = id;
+    this.description = description;
+    this.ghLink = ghLink;
+    this.deployedLink = deployedLink;
+  }
+}
 
 const Projects = () => {
+
+  const portfolioSite = new Project('Portfolio', 'portfolioSite',
+  `The site you're viewing right now is my portfolio. I designed it in Figma and built it using React, Material UI, and Heroku. I look forward to updating it over the years.`, 'https://github.com/davilux/portfolio-site')
+
+  const webby = new Project('Webby', 'webby', 'Webby is a social app designed to connect people to engage in hobbies together.', 'https://github.com/THEM-exclamationpoint/Webby', 'https://webby-social.netlify.app/')
+
+  const magicalMerchants = new Project('Magical Merchants', 'magicalMerchants', 'An e-commerce store selling magical items.', 'https://github.com/Magical-Merchants/magicalMerchants2.0', 'http://magical-merchants.herokuapp.com/')
+
+  const allProjects = [webby, magicalMerchants, portfolioSite]
+
+  const getProjectById = function(id){
+    return allProjects.filter((p)=> p.id === id )[0]
+  }
+
+  const [currentProject, setCurrentProject] = useState(portfolioSite)
+
   return (
     <section id="projects" className="projects">
       <h3>&nbsp;&nbsp;&lt;section className= "projects"&gt;</h3>
@@ -10,32 +37,23 @@ const Projects = () => {
         <h1 className="top-right-h1">Projects</h1>
         {/* <h1 className='left-h1'>02</h1> */}
         <p>
-          The site you're viewing right now is my portfolio. I designed it in
-          Figma and built it using React, Material UI, and Heroku.
-          <br />
-          <br />
-          This was my most ambitious design project to date, and I look forward
-          to updating it over the years.
-          <br />
-          <br />
-          {/* TODO: Link to GitHub for each project. */}
-          <a href="/#projects" className="gh-link">
-            Check out the project GitHub.
-          </a>
+          {currentProject.description}<br /><br />
+          <a href={currentProject.ghLink} className="gh-link" target="_blank" rel="noreferrer noopener">Project GitHub</a><br /><br />
+          {currentProject.deployedLink && <a href={currentProject.deployedLink} className="gh-link" target="_blank" rel="noreferrer noopener">Deployed site</a> }
         </p>
         <div className="projects-container">
-          <div className="project">
-            <FolderIcon />
-            <span> Webby</span>
-          </div>
-          <div className="project">
-            <FolderIcon />
-            <span> Magical Merchants</span>
-          </div>
-          <div className="project">
-            <FolderOpenIcon />
-            <span> Portfolio</span>
-          </div>
+
+          {allProjects.map((p)=> {
+            return (
+              <div className="project" id={p.id} key={p.id} onClick={(e) => {
+                const current = getProjectById(e.target.id)
+                setCurrentProject(current)
+              }}>
+                {currentProject.name === p.name ? <FolderOpenIcon /> : <FolderIcon />}
+                <span>{p.name}</span>
+              </div>
+            )
+          })}
         </div>
       </div>
       <h3>&nbsp;&nbsp;&lt;/section&gt;</h3>
@@ -44,3 +62,14 @@ const Projects = () => {
 }
 
 export default Projects
+
+
+//Any of the project icons/names can be clicked to display more information about that project.
+
+//Project object contains name, info, ghLink and deployedLink.
+
+//currently selected project has an open folder icon.
+
+
+//TODO: Create line spaces within descriptions.
+//TODO: Make it possible to click anywhere on the project div to select a given project
